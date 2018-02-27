4de7c3032ba3e99ab7281234b0f7d7949abb2f73
Darrel Uchendu <2236676u@student.gla.ac.uk>
2018-02-27 00:15:55 +0000

    fixed_media

--- ratemylecturer/models.py
+++ ratemylecturer/models.py
@@ -36,7 +36,6 @@ class Review(models.Model):
     dislikes=models.IntegerField(default=0)
     title=models.CharField(max_length=30)
     review_body=models.CharField(max_length=200, blank=True)
-
  
     def __str__(self):
         return self.title
