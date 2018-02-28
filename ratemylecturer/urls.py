c20774c3d9ccff8718fa073835608d356f83d53f
Yusuf Ogunjobi <1009097o@student.gla.ac.uk>
2018-02-28 14:31:13 +0000

    login view function completed

--- ratemylecturer/urls.py
+++ ratemylecturer/urls.py
@@ -4,7 +4,11 @@ from ratemylecturer import views
 urlpatterns = [
 
 url(r'^$', views.index, name='index'),
-url(r'^register$', views.register, name='register'),
+url(r'^register_student$', views.register_student, name='register_student'),
+url(r'^register_lecturer$', views.register_lecturer, name='register_lecturer'),
+url(r'^login$', views.user_login, name='login'),
+
+
 
 
 ]
--- ratemylecturer/views.py
+++ ratemylecturer/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from ratemylecturer.forms import UserForm,LecturerProfileForm, StudentProfileForm, ReviewForm
 
 
@@ -6,66 +9,77 @@ def index(request):
     return render(request,'ratemylecturer/index.html',{})
 
 # view for registration
-def register(request):
+def register_student(request):
     registered = False; # flag to tell if registration is successful
-    lecturer = False
-    student = False
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
-        if request.POST.get("student"):# student button clicked
-            student = True
-            student_profile_form = StudentProfileForm(data=request.POST)
-            # if userForm and any of the two is valid
-            if user_form.is_valid() and student_profile_form.is_valid():
-                # save user form data
-                user = user_form.save()
-                user.set_password(user.password)
-                user.save();
-                # save student profile data
-                # commit = false until ready to commit to avoid interity problem
-                studentProfile = student_profile_form.save(commit=False)
-                studentProfile.user = user
-                if 'picture' in request.FILES:
-                    studentProfile.picture = request.FILES['picture']
-                studentProfile.save()
-                registered = True
-            else:  # invalid form, for whatever reason
-                print(user_form.errors, student_profile_form.errors)
-         # lecturer button clicked
-        if request.POST.get("lecturer"):# lecturer button clicked
-            lecturer = True
-            lecturer_profile_form = LecturerProfileForm(data=request.POST)
-            if user_form.is_valid() and student_profile_form.is_valid():
-                # save user form data
-                user = user_form.save()
-                user.set_password(user.password)
-                user.save();
+        student_profile_form = StudentProfileForm(data=request.POST)
+        if user_form.is_valid() and student_profile_form.is_valid():
+            # save user form data
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save();
+            # save student profile data
+            # commit = false until ready to commit to avoid interity problem
+            student_profile = student_profile_form.save(commit=False)
+            student_profile.user = user
+            if 'picture' in request.FILES:
+                student_profile.picture = request.FILES['picture']
+            student_profile.save()
+            registered = True
+        else:  # invalid form, for whatever reason
+            print(user_form.errors, student_profile_form.errors)
+    else:  #not http POST
+        user_form = UserForm()
+        student_profile_form = StudentProfileForm()
+    return render( request,'ratemylecturer/register_student.html',{'user_form':user_form,
+                    'student_profile_form':student_profile_form, 'registered': registered})
 
-                lecturerProfile = lecturer_profile_form.save(commit=False)
-                lecturerProfile.user = user
-                if 'picture' in request.FILES:
-                    lecturerProfile.picture = request.FILES['picture']
-                lecturerProfile.save()
-                registered = True
+def register_lecturer(request):
+    registered = False; # flag to tell if registration is successful
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        lecturer_profile_form = LecturerProfileForm(data=request.POST)
+        if user_form.is_valid() and lecturer_profile_form.is_valid():
+            # save user form data
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save();
+            lecturer_profile = lecturer_profile_form.save(commit=False)
+            lecturer_profile.user = user
+            if 'picture' in request.FILES:
+                lecturer_profile.picture = request.FILES['picture']
+            lecturer_profile.save()
+            registered = True
 
-
-            else:#invalid form, for whatever reason
-                print(user_form.errors, lecturer_profile_form.errors)
+        else:#invalid form, for whatever reason
+            print(user_form.errors, lecturer_profile_form.errors)
 
     else:# not http POST
         user_form = UserForm()
-        student_profile_form = StudentProfileForm()
         lecturer_profile_form  = LecturerProfileForm()
-        context_dict = None
-    if student:
-        return render( request,'ratemylecturer/register.html',{'user_form':user_form,'student_profile_form':student_profile_form, 'registered': registered, 'student':
-                            student})
-    if lecturer:
-        return render(request,'ratemylecturer/register.html',{'user_form':user_form,'lecturer_profile_form':lecturer_profile_form, 'registered': registered,
-                            'lecturer':lecturer})
+    return render( request,'ratemylecturer/register_lecturer.html',{'user_form':user_form,'registered': registered, 'lecturer_profile_form':
+                            lecturer_profile_form})
 
-
-
+# login page view
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # password authentication using django
+        user = authenticate(username=username,password=password)
+        if user:
+            if user.is_active: # account may have been disabled
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+            else: # account inactive
+                return HttpResponse("Your account is disabled.")
+        else: # invalid login details
+            print("invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else: # not a HTTP POST, hence blank dictionary object
+        context_dict = {}
+        return render(request,'ratemylecturer/login.html',context_dict)
 
 
 
